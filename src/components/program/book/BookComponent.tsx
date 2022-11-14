94fd3e43e3b3a87aab33677f6a5c7b94a12e752d
kimminyoung <kimminyoung@kimminyoungui-MacBookAir.local>
2022-11-15 00:24:32 +0900

    book,unbook

--- src/components/program/book/BookComponent.tsx
+++ src/components/program/book/BookComponent.tsx
@@ -45,6 +45,44 @@ const BookComponent = () => {
           </button>
         </div>
       </div>
+      <div className="program-wrap">
+        <div className="prg prg-01">
+          <div className="ready-prg">
+            <div className="prg-head sky">
+            <p> 웰검 굿잠 1기 </p>
+              <span className="d-day">
+                D-8
+                <span>예약마감까지</span>
+              </span>
+            </div>
+            <ul className="term">
+              <li className="red">예약기간: 2022.08.01 ~ 2022.08.14</li>
+              <li className="">진행기간: 2022.09.01 ~ 2022.10.01</li>
+              <li className="red">잔여인원: 5명 (모집인원: 15명)</li>
+              <li className="">장소: 서울 안산도시자연공원</li>
+            </ul>
+          </div>
+          <button type="button" className="btn-02">
+            예약하기
+          </button>
+        </div>
+        <div className="prg prg-01">
+          <div className="ready-prg">
+            <div className="prg-head sky">
+              <p> 웰검 굿잠 1기 </p>
+            </div>
+            <ul className="term">
+              <li className="red">예약기간: 2022.08.01 ~ 2022.08.14</li>
+              <li>진행기간: 2022.09.01 ~ 2022.10.01</li>
+              <li className="red">잔여인원: 5명 (모집인원: 15명)</li>
+              <li>장소: 서울 안산도시자연공원</li>
+            </ul>
+          </div>
+          <button type="button" className="btn-02 gray-btn">
+            예약마감
+          </button>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
--- src/components/program/unBook/UnBookComponent.tsx
+++ src/components/program/unBook/UnBookComponent.tsx
@@ -77,6 +77,76 @@ const UnBookComponent = () => {
           </button>
         </div>
       </div>
+      <div className="program-wrap">
+        <div className="prg prg-reserve">
+          <div className="ready-prg">
+            <div className="prg-head unBook sky">
+              <span className="reserve">예약 중</span>
+              <p> 웰검 굿잠 1기 </p>
+            </div>
+            <ul className="term">
+              <li className="red">예약기간: 2022.08.01 ~ 2022.08.14</li>
+              <li className="">진행기간: 2022.09.01 ~ 2022.10.01</li>
+              <li className="red">잔여인원: 5명 (모집인원: 15명)</li>
+              <li className="">장소: 서울 안산도시자연공원</li>
+            </ul>
+          </div>
+          <button type="button" className="btn-02 cnacel-btn">
+            <span>취소하기</span>
+          </button>
+        </div>
+        <div className="prg prg-reserve">
+          <div className="ready-prg">
+            <div className="prg-head unBook sky">
+              <span className="participate reserve-02">참여중</span>
+              <p> 웰검 굿잠 1기 </p>
+            </div>
+            <ul className="term">
+              <li className="red">예약기간: 2022.08.01 ~ 2022.08.14</li>
+              <li className="">진행기간: 2022.09.01 ~ 2022.10.01</li>
+              <li className="red">잔여인원: 5명 (모집인원: 15명)</li>
+              <li className="">장소: 서울 안산도시자연공원</li>
+            </ul>
+          </div>
+          <button type="button" className="btn-02">
+            취소불가
+          </button>
+        </div>
+        <div className="prg prg-reserve">
+          <div className="ready-prg">
+            <div className="prg-head unBook sky">
+              <span className="participate">참여 중</span>
+              <p> 웰검 굿잠 1기 </p>
+            </div>
+            <ul className="term">
+              <li className="red">예약기간: 2022.08.01 ~ 2022.08.14</li>
+              <li className="">진행기간: 2022.09.01 ~ 2022.10.01</li>
+              <li className="red">잔여인원: 5명 (모집인원: 15명)</li>
+              <li className="">장소: 서울 안산도시자연공원</li>
+            </ul>
+          </div>
+          <button type="button" className="btn-02">
+            운영중
+          </button>
+        </div>
+        <div className="prg  prg-reserve prg-reserve-02">
+          <div className="ready-prg">
+            <div className="prg-head unBook sky">
+              <span className="participate">참여 중</span>
+              <p> 웰검 굿잠 1기 </p>
+            </div>
+            <ul className="term">
+              <li className="red">예약기간: 2022.08.01 ~ 2022.08.14</li>
+              <li className="">진행기간: 2022.09.01 ~ 2022.10.01</li>
+              <li className="red">잔여인원: 5명 (모집인원: 15명)</li>
+              <li className="">장소: 서울 안산도시자연공원</li>
+            </ul>
+          </div>
+          <button type="button" className="btn-02 active ">
+            운영중
+          </button>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
