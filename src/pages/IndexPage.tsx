05f56a11a4dd3967273ca3bdc2188cd50577e43f
qusak <qusakdrn@naver.com>
2022-11-15 16:47:12 +0900

    로그인화면 모달 작업

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -5,11 +5,15 @@ import {Link, useNavigate} from "react-router-dom";
 import useAxios from "@hooks/useAxios";
 import {LoginInterface} from "@interfaces/loginInterface";
 import $ from "jquery";
+import ModalComponent from "@components/modal/ModalComponent";
+import {useRecoilState} from "recoil";
+import {modalState} from "@states/modalState";
 
 const Login = () => {
     const navigate = useNavigate();
     const api = useAxios();
     const [buttonShow, setButtonShow] = useState<boolean>(false);
+    const [modal, setModal] = useRecoilState(modalState);
     const [user, setUser] = useState<LoginInterface>({
         userPass: "",
         userID: "",
@@ -34,6 +38,17 @@ const Login = () => {
         }
     };
 
+    const handleLoginCheck = () => {
+        setModal({
+            ...modal,
+            show: true,
+            title: "안내",
+            cancelShow: false,
+            content: <div>아이디 또는 비밀번호를 다시 확인해 주세요.</div>,
+            confirmText: "확인",
+        });
+    };
+
     useEffect(() => {
         let submitButtonShow = false;
         if (user.userID !== "" && user.userPass !== "") submitButtonShow = true;
@@ -41,56 +56,59 @@ const Login = () => {
     }, [user]);
 
     return (
-        <WebLayout>
-            <div className="Login">
+        <React.Fragment>
+            <WebLayout>
+                <div className="Login">
         <span className="Logo">
           <img src="/images/logo.svg" alt="logo"/>
         </span>
-                <div className="Header">
-                    <h2>로그인</h2>
-                </div>
-                <div className="Container loginInput">
-                    <InputElement
-                        type="text"
-                        placeholder="아이디 입력"
-                        name={"userID"}
-                        maxLength="20"
-                        value={user.userID}
-                        onChange={handleChange}
-                        onKeyPress={handleKeyPress}
-                    />
-                    <InputElement
-                        type="text"
-                        placeholder="비밀번호 입력"
-                        name={"userPass"}
-                        maxLength="30"
-                        value={user.userPass}
-                        onChange={handleChange}
-                        onKeyPress={handleKeyPress}
-                    />
-                    <span className="LoginCheck">
+                    <div className="Header">
+                        <h2>로그인</h2>
+                    </div>
+                    <div className="Container loginInput">
+                        <InputElement
+                            type="text"
+                            placeholder="아이디 입력"
+                            name={"userID"}
+                            maxLength="20"
+                            value={user.userID}
+                            onChange={handleChange}
+                            onKeyPress={handleKeyPress}
+                        />
+                        <InputElement
+                            type="text"
+                            placeholder="비밀번호 입력"
+                            name={"userPass"}
+                            maxLength="30"
+                            value={user.userPass}
+                            onChange={handleChange}
+                            onKeyPress={handleKeyPress}
+                        />
+                        <span className="LoginCheck">
             <InputElement type="checkbox"/>
             <label>자동 로그인</label>
           </span>
-          {buttonShow && (
-            <button type="button" className="loginBtn active" onClick={handleLogin}>
-              로그인
-            </button>
-          )}
-          {!buttonShow && (
-            <button type="button" className="loginBtn ">
-              로그인
-            </button>
-          )}
-          <div className="BtnBox">
-            <Link to="/login">회원가입</Link>
-            <Link to="/id">아이디 찾기</Link>
-            <Link to="/password">비밀번호 찾기</Link>
-          </div>
-        </div>
-      </div>
-    </WebLayout>
-  );
+                        {buttonShow && (
+                            <button type="button" className="loginBtn active" onClick={handleLogin}>
+                                로그인
+                            </button>
+                        )}
+                        {!buttonShow && (
+                            <button type="button" className="loginBtn" onClick={handleLoginCheck}>
+                                로그인
+                            </button>
+                        )}
+                        <div className="BtnBox">
+                            <Link to="/login">회원가입</Link>
+                            <Link to="/id">아이디 찾기</Link>
+                            <Link to="/password">비밀번호 찾기</Link>
+                        </div>
+                    </div>
+                </div>
+            </WebLayout>
+            <ModalComponent />
+        </React.Fragment>
+    );
 };
 
 export default Login;
--- src/pages/ProgramList.tsx
+++ src/pages/ProgramList.tsx
@@ -1,38 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import { useRecoilState } from "recoil";
-import { countState, sampleState } from "@states/sampleState";
-import ModalComponent from "@/components/modal/ModalComponent";
-import Form from "react-bootstrap/Form";
+import React from "react";
 import BookComponent from "@/components/program/book/BookComponent";
-import UnBookComponent from "@/components/program/unBook/UnBookComponent";
 import HeaderComponent from "@/components/head/Header";
 import DropBoxComponent from "@/components/program/DropBoxComponent";
-import InputElement from "@/components/elements/InputElement";
-import ProgramDetailComponent from "@/components/program/programDetail/ProgramDetailComponent";
-import ProgramNumberComponent from "@/components/program/ProgramNumberComponent";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { modalState } from "@/states/modalState";
-import $ from "jquery";
-import useAxios from "@/hooks/useAxios";
+import {Link} from "react-router-dom";
 
 const ProgramList = () => {
-
-
-  return (
-    <React.Fragment>
-      <HeaderComponent />
-      <DropBoxComponent />
-      <div className="banner">
-        <Link to={"forest"}>
-          한국산림복지진흥원 <br />
-          산림치유 프로그램 보러가기
-        </Link>
-      </div>
-      <BookComponent />
-      {/*<UnBookComponent />*/}
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <HeaderComponent/>
+            <DropBoxComponent/>
+            <div className="banner">
+                <Link to={"forest"}>
+                    한국산림복지진흥원 <br/>
+                    산림치유 프로그램 보러가기
+                </Link>
+            </div>
+            <BookComponent/>
+        </React.Fragment>
+    );
 };
 
 export default ProgramList;
